6313c8bc67d2af1a22d43aaaab1e2357d56eb630
Naman <naman03surana@gmail.com>
2025-03-06 01:40:49 +0530

    Update server port configuration to use environment variable

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { connect } = require("./db");
 const router = require("./Routes/index");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
