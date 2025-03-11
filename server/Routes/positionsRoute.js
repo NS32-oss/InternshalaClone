0853a390daa1e6fdac6b58318229d29bd080dcc9
Naman <naman03surana@gmail.com>
2025-03-11 23:13:47 +0530

    Remove console log statement for error handling in positionsRoute

--- server/Routes/positionsRoute.js
+++ server/Routes/positionsRoute.js
@@ -48,7 +48,6 @@ router.get("/:id", async (req, res) => {
     }
     res.status(200).json(data);
   } catch (error) {
-    console.log(error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
